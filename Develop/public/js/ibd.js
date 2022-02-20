63f1916b2164bbc1fff470b2bb47cec4baf9c328
Chris Phillips <cp.phillips15@gmail.com>
2022-02-20 18:50:08 -0500

    added logic for indexedDB to upload budget

--- Develop/public/js/ibd.js
+++ Develop/public/js/ibd.js
@@ -34,5 +34,27 @@ function uploadBudget() {
 
     getAll.onsuccess = function() {
         // if there is data in sore, send it to api
-    }
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                budgetObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
 }
