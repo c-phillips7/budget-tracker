19095ab4f9b7221da8c4acbdc3d8f790ea8394c9
Chris Phillips <cp.phillips15@gmail.com>
2022-02-20 19:41:34 -0500

    added favicon npm package to solve heroku issue

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const favicon = require('express-favicon');
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
+
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use(logger("dev"));
 
@@ -26,4 +29,8 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+});
+
+const server = app.listen(3000, function(){
+  console.log('server is running at %s .', server.address().port);
 });
